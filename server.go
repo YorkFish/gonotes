a4c18f7bb9f1b22292d8d4b44f8c008af915c6c0
york <yorkjiashang@gmail.com>
2025-08-17 10:57:05 +0800

    [Add] v2, add user.go; add online function, broadcast function

--- server.go
+++ server.go
@@ -3,25 +3,72 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	Ip   string
 	Port int
+
+	// 在线用户的列表
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+
+	// 消息广播的 channel
+	Message chan string
 }
 
 // 创建一个 server 的接口
 func NewServer(ip string, port int) *Server {
 	server := &Server{
-		Ip:   ip,
-		Port: port,
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
 	}
 	return server
 }
 
+// 监听 Message 广播消息 channel 的 goroutine
+// 一旦有消息就发送给全部的在线 User
+func (this *Server) ListenMessager() {
+	for {
+		msg := <-this.Message
+
+		// 将 msg 发送给全部的在线 user
+		this.mapLock.Lock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- msg
+		}
+		this.mapLock.Unlock()
+	}
+}
+
+// 创建一个 server 的接口
+func (this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+
+	this.Message <- sendMsg
+}
+
 func (this *Server) Handler(conn net.Conn) {
 	// 当前连接的业务
-	fmt.Println("连接建立成功")
+	// fmt.Println("连接建立成功")
+
+	user := NewUser(conn)
+
+	// 用户上线，将用户加入到 onlinemap
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+
+	// 广播当前用户上线消息
+	this.BroadCast(user, "已上线")
+
+	// 当前 handler 阻塞
+	select {
+		
+	}
 }
 
 // 启动服务器的接口
@@ -35,6 +82,9 @@ func (this *Server) Start() {
 
 	// close listen socket
 	defer listener.Close()
+
+	// 启动监听 Message 的 goroutine
+	go this.ListenMessager()
 
 	for {
 		// accept
